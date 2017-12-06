1ae2679a52fe3df3b488e2c11e6212c0fef255b0
minerstat <hello@coinscrow.io>
2017-12-06 09:27:14 -0500

    Set config.js to default

--- config.js
+++ config.js
@@ -1,18 +1,3 @@
-////////////////////////////////////////////////////////////////////////////////////////////
-// MINERSTAT.COM - LINUX CLIENT BETA
-
-// EDIT THESE LINES BELOW TO CONFIGURE YOUR WORKER
-
-global.accesskey = "CHANGEIT"; // minerstat.com LOGIN key
-global.worker = "CHANGEIT";   // your created worker at the website
-
-// global.client EXAMPLES:
-// NVIDIA: ewbf-zec
-// AMD: claymore-zec
-// NVIDIA & AMD: claymore-eth, claymore-etc, claymore-exp, claymore-music, claymore-ubq
-// claymore-eth ALGO & DUAL-MINING ALGO: algo
-
-global.path = __dirname; // DO NOT TOUCH THIS !! __dirname is the correct varible!
-
-////////////////////////////////////////////////////////////////////////////////////
-/////// config.js END /////////////////////////////////////////////////////////////
+global.accesskey = "CHANGEIT"; 
+global.worker = "CHANGEIT";   
+global.path = __dirname;
