30f16b45d2a34b162b9eef8f7af71b734ee131c3
coinscrow <hello@coinscrow.io>
2017-12-21 12:36:56 +0100

    Monitor prepared for the next version

--- monitor.js
+++ monitor.js
@@ -10,21 +10,36 @@ detect: function () {
 
 var exec = require('child_process').exec;
 var child;
+
+global.gputype = "unknown";
   
-child = exec("lshw -C display",
+child = exec("nvidia-smi -L",
      function (error, stdout, stderr) {
        var response = stdout;
-        global.gputype = "unknown";
-        if(response.indexOf("NVIDIA") > -1) {
+
+        if(response.indexOf("GPU 0:") > -1) {
           global.gputype = "nvidia";
         } 
-        if(response.indexOf("AMD") > -1) {
-          global.gputype = "amd";
-        } 
+  
         if (error !== null) {
             console.log('exec error: ' + error);
         }
 });
+
+child = exec("bin/amdmeminfo",
+     function (error, stdout, stderr) {
+      var response = stderr;
+
+        if(response.indexOf("no version information") === -1) {
+          global.gputype = "amd";
+        } 
+  
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+});
+
+
 },
 
 ////////////////////////////////////////////////////////////////////////////////////////////
@@ -32,12 +47,27 @@ child = exec("lshw -C display",
 
 HWamd: function () {
 
+// TO DO AS NVIDIA
+// AMDMEMINFO QUERY
+// POST DATA TO THE SERVER
+
 },
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 // NVIDIA-SMI (for GPU Temp/Fan/ ... monitoring)
 
+// gpucount > nvidia-smi --query-gpu=count --format=csv,noheader
+// fans > nvidia-smi -i 0 --query-gpu=fan.speed --format=csv,noheader
+// temp > nvidia-smi -i 0 --query-gpu=temperature.gpu --format=csv,noheader
+
 HWnvidia: function () {
+
+// TO DO
+// CREATE LOOP EVERY MINUTE
+// WHILE X != GPUCOUNT
+// FAN, TEMP to array
+// JSON 
+// SEND DATA TO THE SERVER
 
 }
 
