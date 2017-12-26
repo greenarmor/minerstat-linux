b78e38fcb3116f0a73754d32fff8242fbd92340f
minerstat <hello@coinscrow.io>
2017-12-27 00:16:03 +0100

    Minor bug fix

--- tools.js
+++ tools.js
@@ -130,7 +130,7 @@ console.log(res);
 
 console.log(colors.magenta("/*/*/*/*/*/*/*/*/*/*/*/*/*/*/"));
 console.log(colors.magenta("AUTOUPDATE: DONE"));
-if(global.client.indexOf("Already up-to-date") === -1) {
+if(global.client.indexOf("Already up-to-date") === 0) {
 console.log(colors.red("NODE RESTART NEEDED! Press Ctrl + C to exit"));
 }
 console.log(colors.magenta("*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/"));
