9db1f64681b42e1ea221700cf9ffd8e879de86ea
coinscrow <hello@coinscrow.io>
2017-12-29 13:30:40 +0100

    Install Script, Alias, BG Mod

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get --assume-yes update
-sudo apt-get --assume-yes install git nodejs unzip npm ocl-icd-opencl-dev libcurl3:i386 libcurl4-openssl-dev gcc-4.9 libssl-dev libjansson-dev libpci-dev
+sudo apt-get --assume-yes install git screen nodejs unzip npm ocl-icd-opencl-dev libcurl3:i386 libcurl4-openssl-dev gcc-4.9 libssl-dev libjansson-dev libpci-dev
 sudo apt-get --assume-yes upgrade libstdc++6
 git clone https://github.com/coinscrow/minerstat-linux/
 cd minerstat-linux
@@ -22,10 +23,19 @@ global.path = __dirname;" >> config.js
 
 chmod -R 777 *
 
+echo "alias minerstat-start-bg='cd "$(pwd)"/minerstat-linux; screen -A -m -d -S minerstat-console node start;'" >> ~/.bashrc 
+echo "alias minerstat-console='screen -x minerstat-console;'" >> ~/.bashrc
+echo "alias minerstat-start='cd "$(pwd)"/minerstat-linux; node start;'" >> ~/.bashrc 
+echo "alias minerstat-stop='cd "$(pwd)"/minerstat-linux; node stop;'" >> ~/.bashrc 
+exec bash
+source ~/.bashrc
+
 echo " "
-echo "Enter: cd minerstat-linux"
+echo "!!!! INFO !!!!!"
+echo "Start mining with output: minerstat-start"
+echo "Start mining in the background: minerstat-start-bg"
 echo " "
-echo "After run the client with: node start"
-echo " "
-echo " "
-echo "INSTALLATION DONE"
+echo "View console in background mode: minerstat-console"
+echo "Stop mining and minerstat: minerstat-stop"
+echo "!!!! INSTALLATION DONE !!!!!"
+
