dc73227fb31cc82227cecd2a3e20e07bd7cbcee0
minerstat <hello@coinscrow.io>
2018-01-16 11:24:09 +0100

    install test

--- install.sh
+++ install.sh
@@ -1,11 +1,101 @@
 #!/bin/bash
+
+echo "
+MZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ$ZMMMMMMMMMMMMMMMMMMMMMMM
+ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ$MMMMMMMMMMMMMMMMMMMMMM
+ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZMMMMMMMMMMMMMMMMMMMMMM
+ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ$MMMMMMMMMMMMMMMMMMMMMM
+ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZOMMMMMMMMMMMMMMMMMMMMMMM
+ZZZZZZZZZOMMMMMMMMMZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+ZZZZZZZZZOMMMMMMMMMMZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ$ZDMMMMMMMMMMMMMMMMMMMMMMMMM
+ZZZZZZZZZOMMMMMMMMMMMZZZZZZZZZZZZZZZZZ$ZZZZZZZZZZ$ZZZZZ$$MMMMMMMMMMMMMMMMMMMMMMM
+ZZZZZZZZZOMMMMMMMMMMMMZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZDMMMMMMMMMMMMMMMMMMMMMMM
+ZZZZZZZZZOMMMMMMMMMMMMMZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZMMMMMMMMMMMMMMMM$$$$OMMM
+ZZZZZZZZZOMMMMMMMMMMMMMMZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ$MMMMMMMMMMMMMMMMMZ$$$$$$M
+ZZZZZZZZZOMMMMMMMMMMMMMM8ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZMMMMMMMMMMMMM$MMMMM$$$$$$M
+ZZZZZZZZZOMMMMMMMMMMMMMMMOZZZZ$ZZZZZZZZ$ZZZZZZZZZZZZZMMMMMMMMMMMMM$$$$$MM$ZZZZZZ
+ZZZZZZZZZOMMMMMMMMMMMMMMMM$ZZZZZZZZZZZZZZZZZZZZZZZZZMMMMMMMMMMMMM$$M$$$$ZZ$$$$ZZ
+ZZZZZZZZZOMMMMMMMMMMMMMMMMMZZZZZZZZ$ZZZZZZZZZZZZZZZMMMMMMMMMMMMM$$MM$$$$$$$$$$$$
+ZZZZZZZZZOMMMMMMMMMMMMMMMMMMZZZZZZZZZZZZZ$ZZZZZZZZMMMMMMMMMMMMM$$NMM$$$$$$$$$$$$
+ZZZZZZZZZOMMMMMMMMMMMMMMMMMMM$ZZZZ$ZZZZZZZZZZZZZZMMMMMMMMMMMMM$$DMMM$$$$$$$$$$$$
+ZZZZZZZZZOMMMMMMMMMMMMMMMMMMMM$Z$ZZZZZZZZZZZZZZZNMMMMMMMMMMMM$$OMMMM$$$$$$$$$$$$
+ZZZZZZZZZOMMMMMMMMMMMMMMMMMMMMMZZZZZZZZZ$ZZZZZZZMMMMMMMMMMMMD$$MMMMM$$$$$$$$$$$$
+ZZZZZZZZZOMMMMMMMMMMMMMMMMMMMMMMZZZZZZZZZZZZZZ$MMMMMMMMMMMMM$$MMMMMM$$$$ZZZZ$$$$
+ZZZZZZZZZOMMMMMMMMMMMMMMMMMMMMMMMZZZZZZZZZZZZZMMMMMMMMMMMMM$ZMMMMMMM$$$$ZZZZZ$$Z
+ZZZZZZZZZOMMMMMMMMMMMMMMMMMMMMMMMMZZZZZZZZZZ$MMMMMMMMMMMMM$$MMMMMMMM$$ZZZZZZZ$$$
+ZZZZZZZZZOMMMMMMMMMMMMMMMMMMMMMMMMMZZZZZZZZZMMMMMMMMMMMMM$$MMMMMMMMM$$$$$ZZZZZZZ
+ZZZZZZZZZOMMMMMMMMMMM$MMMMMMMMMMMMMDZZZZZZZMMMMMMMMMMMMM$$MMMMMMMMMM$$$$$$$$ZZZZ
+ZZZZZZZZZOMMMMMMMMMMM$$MMMMMMMMMMMMM8$ZZZ$MMMMMMMMMMMMM$$MMMMMMMMMMM$$$$$$$$$$$$
+ZZZZZZZZZOMMMMMMMMMMM$$$MMMMMMMMMMMMMZZZZMMMMMMMMMMMMM$$MMMMMMMMMMMM$$$$$$$$$$$$
+ZZZZZZZZZOMMMMMMMMMMM$$$$MMMMMMMMMMMMM$$NMMMMMMMMMMMM$$$MMMMMMMMMMMM$$$$$$$$$$$$
+ZZZZZZZZZOMMMMMMMMMMM$$$$$MMMMMMMMMMMMMNMMMMMMMMMMMM$$$$MMMMMMMMMMMM$$$$$$$$$$$$
+ZZZZZZZZZOMMMMMMMMMMM$$$$$$MMMMMMMMMMMMMMMMMMMMMMMM$$$$$MMMMMMMMMMMM$$$$$$$$$$$$
+ZZZZZZZZZOMMMMMMMMMMM$$$$$$$MMMMMMMMMMMMMMMMMMMMMM$$$$$$MMMMMMMMMMMM$$$$ZZ$$$$$$
+ZZZZZZZZZOMMMMMMMMMMM$$$$$$$ZMMMMMMMMMMMMMMMMMMMM$$$$$$$MMMMMMMMMMMM$$$$$ZZZZ$$$
+ZZZZZZZZZOMMMMMMMMMMM$$$$$$$$$MMMMMMMMMMMMMMMMMM$$$$$$Z$MMMMMMMMMMMM$$$$$$$Z$ZZZ
+ZZZZZZZZZOMMMMMMMMMMM$$$$$$Z$$ZMMMMMMMMMMMMMMMMZ$$$$$$$ZMMMMMMMMMMMMZ$$Z$$$$$$$$
+ZZZZZZZZZOMMMMMMMMMMM$$$$$$ZZ$$$MMMMMMMMMMMMMM$$$$$$$$$$MMMMMMMMMMMM$$$ZZZZ$$$$$
+ZZZZZZZZZOMMMMMMMMMMMZ$$$$$$ZZ$$$MMMMMMMMMMMMO$$$$$$$$$$MMMMMMMMMMMM$$$$$ZZZZZ$$
+OZZZZZZZZOMMMMMMMMMMM$$$$$$$Z$$$$$NMMMMMMMMMD$$$Z$ZZ$$$$MMMMMMMMMMMM$$$$$$$$ZZZZ
+MMDZ$$$$$OMMMMMMMMMMM$$$$$$ZZ$$$$$$DMMMMMMMM$$$$$$ZZZZ$$$$$$$$$$$$ZZ$$$$$$$$$$$$
+MMMMMMMMMMMMMMMMMMMMM$$$$$$ZZ$$$$$$$$$$$$$$$$$$$$$$$ZZZ$$$$$$$$$$$$ZZZ$$$$$$$$$$
+MMMMMMMMMMMMMMMMMMMMM$$$$$$$Z$$$$$$$$$$$$$$$$$$$$$$$$$ZZ$$$$$$$$$$$$ZZZ$$$$$$$$$
+MMMMMMMMMMMMMMMMMMMMM$$$$$$$Z$$$$$$$$$$$$ZZ$$$$Z$$$$$$$ZZ$$$$$$$$$$$$ZZZZ$$$$$$$
+MMMMMMMMMMMMMMMMMMMMM$$$$$$$$ZZ$$$$$$$$$$$$$ZZZZ$$$$$$$$$ZZ$Z$$$$$$$$$$ZZZ$$$$$$
+MMMMMMMMMMMMMMMMMMMMM$$$$$$$$$Z$$$Z$$$$$$$$$$ZZZ$$$$$$$$$$$ZZZ$$$$$$$$$$ZZZ$$$$$
+MMMMMMMMMMMMMMMMMMMMM$$$$$$$$$$Z$$$Z$$$$$$$$$$$ZZZ$$$$$$$$$$ZZZZ$$$$$$$$$ZZZ$$$$
+MMMMMMMMMMMMMMMMMMMMM$$$$$$$$$$$$$$$Z$$$$$$$$$$$ZZZZ$$$$$$$$$$ZZZZ$$$$$$$$ZZ$$$$
+MMMMMMMMMMMMMMMMMMMMM$$$$$$$$$$$Z$$ZZZZ$$$$$$$$$$$ZZZ$$$$$$$$$$$ZZZZ$$$$$$$$ZZ$N
+MMMMMMMMMMMMMMMMMMMMMM$$$$$$$$$$ZZZ$ZZZZ$$$$$$$$$$$ZZZ$$$$$$$$$$$ZZZZ$$$$$$$$$NM
+MMMMMMMMMMMMMMMMMMMMMMMMD$$$$$$$$ZZ$$ZZZZ$$$$$$$$$$$ZZZZ$$$$$$$$$$$ZZZ$$$$$ZMMMM"
+
+echo "Minerstat Linux Installer"
+
+echo -n "Nvidia or AMD (a/n)? "
+read answer
+if echo "$answer" | grep -iq "^n" ;then
+    GPUTYPE="nvidia"
+else
+    GPUTYPE="amd"
+fi
+
+echo "Your selected gpu type: $answer"
+echo ""
+
+echo -n "Install Graphics Drivers (y/n)? "
+read answerz
+if echo "$answerz" | grep -iq "^y" ;then
+    DRIVER="yes"
+else
+    DRIVER="no"
+fi
+
+echo "Install Graphics Drivers? $answer"
+echo ""
+
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo add-apt-repository ppa:graphics-drivers/ppa -y
 sudo apt-get --assume-yes update
 sudo apt-get --assume-yes install git screen nodejs nodejs-legacy unzip npm ocl-icd-opencl-dev libcurl3:i386 libcurl4-openssl-dev libssl-dev libjansson-dev libpci-dev
 sudo apt-get --assume-yes upgrade libstdc++6
 git clone https://github.com/coinscrow/minerstat-linux/
 cd minerstat-linux
 npm install colors sleep pump request express sphp progress ascii-text-generator fkill needle parse-spawn-args
+
+
+if [ DRIVER = "yes" ]; then
+  
+if [ GPUTYPE = "nvidia" ]; then
+sudo apt-get install nvidia-cuda-toolkit -y
+sudo update-grub
+sudo rm /var/crash/*
+sudo nvidia-xconfig -s -a --force-generate --allow-empty-initial-configuration --cool-bits=28 --registry-dwords="PerfLevelSrc=0x2222" --no-sli --connected-monitor="DFP-0"
+fi
+  
+else
+  echo "Graphics Driver Insaller Skipped"
+fi
+
 
 echo " "
 echo "Please enter your minerstat.com AccessKey: "
@@ -29,13 +119,29 @@ echo "alias minerstat-start='cd "$(pwd)"/; node start;'" >> ~/.bashrc
 echo "alias minerstat-stop='cd "$(pwd)"/; node stop;'" >> ~/.bashrc 
 
 echo " "
-echo "!!!! INFO !!!!!"
+echo "!!!! INSTALLATION DONE !!!!!"
 echo "Start mining with output: minerstat-start"
 echo "Start mining in the background: minerstat-start-bg"
 echo " "
 echo "View console in background mode: minerstat-console"
 echo "Stop mining and minerstat: minerstat-stop"
 echo "!!!! INSTALLATION DONE !!!!!"
+echo ""
+
+if [ DRIVER = "yes" ]; then
+
+echo "-------- WARNING --------"
+echo "You need to reboot your computer to apply graphics driver changes."
+echo ""
+
+echo -n "Do you want to REBOOT now (y/n)? "
+read rboot
+
+fi
 
 exec bash
 source ~/.bashrc
+
+if echo "$rboot" | grep -iq "^y" ;then
+    reboot -f
+fi
